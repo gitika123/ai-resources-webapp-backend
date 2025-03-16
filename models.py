8ea257a69417cd1bb3fb518574bd2ea2081ffe0e
Sahil-Mhatre8201 <sahil.mhatre@sjsu.edu>
2025-03-15 19:56:02 -0700

    complete user upload, admin and by the community feature

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, TIMESTAMP, UniqueConstraint
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, TIMESTAMP, UniqueConstraint, Boolean
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from database import Base
@@ -9,8 +9,11 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+    is_admin = Column(Boolean, default=False)  # Add the is_admin field
+
 
     bookmarks = relationship("Bookmark", back_populates="user")
+    uploads = relationship("CommunityUpload", back_populates="user")
 
 class Bookmark(Base):
     __tablename__ = "bookmarks"
@@ -23,4 +26,18 @@ class Bookmark(Base):
     user_id = Column(Integer, ForeignKey("users.id"))  # Foreign key to associate with user
 
     user = relationship("User", back_populates="bookmarks")  # Change 'owner' to 'user'
+
+class CommunityUpload(Base):
+    __tablename__ = "community_uploads"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    resource_type = Column(String, nullable=False)  # e.g., 'GitHub', 'Course', 'Blog', 'Research Paper'
+    status = Column(String, default="pending_approval")  # Can be 'pending_approval' or 'approved'
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)  # Foreign key to link the user
+    url = Column(String, nullable=False)  # New field for storing URL
+
+    user = relationship("User", back_populates="uploads")  # Fix the relationship here
+
 
